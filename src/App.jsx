0aac8612a90dc21265d4fbe50949e84e8b8f4086
SniOpy <soufianeibna@gmail.com>
2024-11-21 12:58:59 +0100

    ✨ F01: LOGIN > Create LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
+import LoginPage from "./components/pages/LoginPage";
 
 
-  return <div>App</div>
+export default function App() {
+  //! state
+  
+  //! comportements
+  
+  //! render
+
+  return <LoginPage/>
 }
 
-export default App
