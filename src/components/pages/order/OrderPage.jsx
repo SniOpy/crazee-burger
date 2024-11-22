2b77f7e0cd0a2dfcb094a01692057e4fdda65b88
SniOpy <soufianeibna@gmail.com>
2024-11-22 23:20:05 +0100

    ✨ F02: LOGIN > add redirection from/to OrderPage

---
+++ src/components/pages/order/OrderPage.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+  export default function OrderPage() {
+    return (
+      <div>
+        <h1>Bonjour Bob</h1>
+        <br/>
+        <Link to="/"><button>Déconnexion</button></Link>
+      </div>
+    )
+  }
