5f2986110d3ff736b700332c0600312e8df3f2a2
SniOpy <soufianeibna@gmail.com>
2024-11-26 22:46:52 +0100

    📦 F03: Add styled-components

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -2,34 +2,34 @@ import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 
 export default function LoginForm() {
-     //! state
-     const [username, setUsername] = useState("")
-     const navigate = useNavigate()
-     //! comportements
-     const handleSubmit = (event) => { 
-         event.preventDefault();
-         navigate(`order/${username}`)
-      }
+  //! state
+  const [username, setUsername] = useState("")
+  const navigate = useNavigate()
+  //! comportements
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate(`order/${username}`)
+  }
 
-      const handleChange = (event) => { 
-        setUsername(event.target.value)
-      }
+  const handleChange = (event) => {
+    setUsername(event.target.value)
+  }
 
-    //! render
+  //! render
   return (
     <>
-        <h1>Bienvenue chez nous !</h1>
-        <br />
-        <h2>Connectez-vous</h2>
-        <form action="submit" onSubmit={handleSubmit}>
-            <input 
-              type="text" 
-              onChange={handleChange}  
-              placeholder="Entrer votre nom..."
-              required="required"
-            />
-            <button type='submit'>Accéder à votre espace</button>
-        </form>
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={handleChange}
+          placeholder="Entrer votre nom"
+          required="required"
+        />
+        <button type='submit'>Accéder à mon espace</button>
+      </form>
     </>
   )
 }
