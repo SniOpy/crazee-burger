83f271f169d556c67286f9062f1360fdc93b4eb9
SniOpy <soufianeibna@gmail.com>
2024-11-27 10:03:44 +0100

    ✨F03: LOGIN > Add background & logo

--- src/components/reusable-ui/Logo.jsx
+++ src/components/reusable-ui/Logo.jsx
@@ -1,14 +1,38 @@
 import styled from "styled-components";
+import { theme } from "../theme/index";
 
 export default function Logo() {
     return (
         <LogoStyled>
-            Logo
+            <h1>CRAZEE</h1>
+            <img src="/images/logo-orange.png" alt="logo crazee burger" />
+            <h1>BURGER</h1>
         </LogoStyled>
     )
 }
 
 const LogoStyled = styled.div`
-    background: blue;
-    
+    display:flex;
+    align-items: center;
+    transform : scale(2.5);
+
+    h1{
+        display: inline;
+        font-family: 'Amatic SC', cursive;
+        color: ${theme.colors.primary};
+        font-size : ${theme.fonts.P4};
+        letter-spacing: 1.5px;
+        line-height: 1em;
+        font-weight: ${theme.weights.bold};
+        text-align: center;
+        text-transform: uppercase;
+    }
+
+    img{
+        object-fit: contain;
+        object-position: center;
+        height: 60px;
+        width: 80px;
+        margin: 0 5px;    
+    }
 `;
