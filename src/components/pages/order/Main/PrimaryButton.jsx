18d7e66dd47c4f46fc56c69a9189fcd310779580
SniOpy <soufianeibna@gmail.com>
2024-12-11 15:01:42 +0100

    ♻️ F05: ORDER > CARD > Add <PrimaryButton/> and reusable component

---
+++ src/components/pages/order/Main/PrimaryButton.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import { theme } from "../../../theme";
+
+export default function PrimaryButton({label}) {
+  return (
+    <PrimaryButtonStyled 
+        type="button">
+        {label}
+    </PrimaryButtonStyled>
+  )
+}
+
+const PrimaryButtonStyled = styled.button`
+  
+            background-color: ${theme.colors.primary};
+            color:#fff;
+            border: none;
+            width: 95px;
+            height: 38px;
+            border-radius: ${theme.borderRadius.round};
+            font-weight: ${theme.weights.medium};
+`;
