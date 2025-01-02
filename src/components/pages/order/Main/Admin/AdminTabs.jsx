19fc9f42df5f1d9305c7d7dc55be997c09a02187
SniOpy <soufianeibna@gmail.com>
2025-01-02 23:12:19 +0100

    💄 F07-test: ORDER > Tabs > Add Tab

--- src/components/pages/order/Main/Admin/AdminTabs.jsx
+++ src/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -1,14 +1,15 @@
 import styled from "styled-components";
 import Tab from "../../../../reusable-ui/Tab";
+import { FiChevronDown } from "react-icons/fi";
 
 export default function AdminTabs() {
   return (
-    <AdminTabsStyled><Tab/></AdminTabsStyled>
+    <AdminTabsStyled><Tab Icon={<FiChevronDown />} /></AdminTabsStyled>
   )
 }
 
 const AdminTabsStyled = styled.div`
-  border: 1px solid red;
+  padding-left: 40px;
 `;
 
 
--- src/components/reusable-ui/Tab.jsx
+++ src/components/reusable-ui/Tab.jsx
@@ -1,9 +1,46 @@
-export default function Tab() {
+import styled from 'styled-components';
+import { theme } from '../../theme'
+
+
+export default function Tab({ Icon }) {
   return (
-    <TabStyled>Tab</TabStyled>
+    <TabStyled>{Icon}</TabStyled>
   )
 }
 
 const TabStyled = styled.button`
-  
+
+ padding: 0 22px;
+ height: 43px;
+ box-shadow: 0px -6px 8px -2px rgba(0, 0, 0, 0.1);
+ background-color: ${theme.colors.white};
+ border:solid;
+ cursor: pointer;
+
+ display: flex;
+ justify-content: center;
+ align-items: center;
+
+ //! position
+ position: relative;
+ left:0%;
+ top: 1px; 
+
+ //!fonts
+ font-size: ${theme.fonts.size.P0};
+ color: ${theme.colors.greyLight};
+ 
+ //! border
+ border-width: 1px, 1px, 2px, 1px;
+ border-style: solid;
+ border-color: rgba(228, 229, 233, 1);
+
+ //! border-radius
+  border-radius: ${theme.borderRadius.round};
+  border-bottom-right-radius: 0px;
+  border-bottom-left-radius: 0px;
+
+  &:hover {
+  border-bottom: 2px solid  ${theme.colors.white};
+  }
 `;
