87316b4d9b0bec19f89ae6f95e9adc23d1cf2850
SniOpy <soufianeibna@gmail.com>
2025-01-05 23:11:28 +0100

    🩹 F07: Add ne documents to F07

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-    import { Route, Routes } from "react-router-dom";
-    import LoginPage from "./components/pages/login/LoginPage";
-    import ErrorPage from "./components/pages/error/ErrorPage";
-    import OrderPage from "./components/pages/order/OrderPage";
+import logo from "./logo.svg"
+import "./App.css"
+import LoginPage from "./components/pages/login/LoginPage"
+import { Route, Routes } from "react-router-dom"
+import OrderPage from "./components/pages/order/OrderPage"
+import ErrorPage from "./components/pages/error/ErrorPage"
 
-    export default function App() {
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/order/:username" element={<OrderPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  )
+}
 
-            return (
-            <Routes>
-              <Route path="/" element={<LoginPage/>}></Route>
-              <Route path="/order/:username" element={<OrderPage/>}></Route>
-              <Route path="*" element={<ErrorPage/>}></Route>
-            </Routes>
-            )
-  }
-
+export default App
--- src/components/pages/error/ErrorPage.jsx
+++ src/components/pages/error/ErrorPage.jsx
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
 
 export default function ErrorPage() {
   return (
     <div>
-        <h1>ErrorPage</h1>
-        <br/>
-        <Link to="/"><button>Retourner vers la page d'accueil</button></Link>
-      </div>
+      <h1>ErrorPage</h1>
+      <Link to="/">
+        <button>Retourner vers la page d'accueil</button>
+      </Link>
+    </div>
   )
 }
