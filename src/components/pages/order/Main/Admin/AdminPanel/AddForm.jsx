d8e058386078376352c72c4ea5109e2f2c5a452f
SniOpy <soufianeibna@gmail.com>
2025-01-13 02:30:05 +0100

    ♻️F08-test: order > AddForm > use of property name

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -2,51 +2,64 @@ import { useContext, useState } from "react";
 import styled from "styled-components";
 import OrderContext from "../../../../../../context/OrderContext";
 
-
+const EMPTY_PRODUCT = {
+  id: "",
+  title: "",
+  imageSource: "",
+  price: 0,
+};
 
 export default function AddForm() {
-
-  const [title, setTitle] = useState("");
-  const [imageSource, setImageSource] = useState("");
-  const [price, setPrice] = useState(0);
   const { addProduct } = useContext(OrderContext);
 
-  const newProduct = {
-    id: new Date().getTime(),
-    title,
-    imageSource,
-    price,
-  }
+  const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT);
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    addProduct(newProduct);
-  }
+    const productToAdd = {
+      ...newProduct,
+      id: new Date().getTime(),
+    };
+    addProduct(productToAdd);
+  };
 
-  const handleTitleChange = (event) => {
-    setTitle(event.target.value);
-  }
-
-  const handleImageChange = (event) => {
-    setImageSource(event.target.value);
-  }
-
-  const handlePriceChange = (event) => {
-    setPrice(event.target.value);
-  }
+  const handleChange = (event) => {
+    const newValue = event.target.value;
+    const nameInput = event.target.name;
+    setNewProduct({ ...newProduct, [nameInput]: newValue });
+  };
 
   return (
     <AddFormStyled onSubmit={handleSubmit}>
       <div className="image-preview">Image Preview</div>
       <div className="input-fields">
-        <input type="text" placeholder="Nom du produit (ex: Super Burger)" value={title} onChange={handleTitleChange}/>
-        <input type="text" placeholder="Lien URL d'une image (ex: https://la-photo-de-mon-produit.png)" value={imageSource} onChange={handleImageChange}/>
-        <input type="text" placeholder='Ex: "2119,56", et "2119.56"' value={price} onChange={handlePriceChange}/>
+        <input
+          name="title"
+          type="text"
+          placeholder="Nom du produit (ex: Super Burger)"
+          value={newProduct.title}
+          onChange={handleChange}
+        />
+        <input
+          name="imageSource"
+          type="text"
+          placeholder="Lien URL d'une image (ex: https://la-photo-de-mon-produit.png)"
+          value={newProduct.imageSource}
+          onChange={handleChange}
+        />
+        <input
+          name="price"
+          type="text"
+          placeholder='Ex: "2119,56", et "2119.56"'
+          value={newProduct.price}
+          onChange={handleChange}
+        />
       </div>
-      <div className="button-submit">       
-         <button type="submit">Button</button>
+      <div className="button-submit">
+        <button type="submit">Button</button>
       </div>
     </AddFormStyled>
-  )
+  );
 }
 
 const AddFormStyled = styled.form`
@@ -55,27 +68,22 @@ const AddFormStyled = styled.form`
   display: grid;
   height: 100%;
   width: 70%;
-  grid-template-rows: repeat(4,1fr);
+  grid-template-rows: repeat(4, 1fr);
   grid-template-columns: 1fr 3fr;
 
-
-  .image-preview{
-    background:red;
+  .image-preview {
+    background: red;
     grid-area: 1 / 1 / 3 / 2;
   }
-  .input-fields{
-    background:blue;
+  .input-fields {
+    background: blue;
     grid-area: 1 / 2 / 3 / 3;
-    display:grid;
-
+    display: grid;
   }
-  .button-submit{
-    background:green;
+  .button-submit {
+    background: green;
     grid-area: 3 / 2 / 3 / -1;
-    display:grid;
-    width:50%;
-
+    display: grid;
+    width: 50%;
   }
 `;
-
-
