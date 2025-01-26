ac8803d80e3c490f251fd61a04960c12784ba027
SniOpy <soufianeibna@gmail.com>
2025-01-26 23:00:40 +0100

    ✨ F09-test: ORDER > EditForm > Add text if no products selected

--- src/components/pages/order/Main/Admin/AdminPanel/EditForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/EditForm.jsx
@@ -1,5 +1,25 @@
+import styled from "styled-components";
+import { theme } from "../../../../../../theme";
+import { HiCursorClick } from "react-icons/hi";
+
 export default function EditForm() {
   return (
-    <div>EditForm</div>
-  )
+    <EditFormStyled>
+      <span>
+        Cliquez sur un produit pour le modifier <HiCursorClick />
+      </span>
+    </EditFormStyled>
+  );
 }
+
+const EditFormStyled = styled.div`
+  /* border: 1px solid red; */
+  padding: 10px 5%;
+  display: flex;
+  /* align-items: center; */
+  font-family: "Amatic SC", cursive;
+  font-size: ${theme.fonts.size.P3};
+  font-weight: ${theme.fonts.weights.regular};
+  color: rgba(116, 123, 145, 1);
+  margin-top: 81px;
+`;
