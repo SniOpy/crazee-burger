8a55cf2240d4e2ccc4572de52be0c66baf71340c
SniOpy <soufianeibna@gmail.com>
2025-02-03 11:24:15 +0100

    🚧 F09-test: ORDER > Card > Add shadows in design system

--- src/components/reusable-ui/Card.jsx
+++ src/components/reusable-ui/Card.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { theme } from "../../theme";
 import Button from "./Button";
 import { TiDelete } from "react-icons/ti";
@@ -12,29 +12,36 @@ export default function Card({
   hasDeleteButton,
   className,
   onClick,
+  isHoverable,
 }) {
   return (
-    <CardStyled className={className} onClick={onClick}>
-      {hasDeleteButton && (
-        <button
-          className="delete-button"
-          aria-label="delete-button"
-          onClick={onDelete}
-        >
-          <TiDelete className="icon" />
-        </button>
-      )}
+    <CardStyled
+      className={className}
+      onClick={onClick}
+      isHoverable={isHoverable}
+    >
+      <div className="card">
+        {hasDeleteButton && (
+          <button
+            className="delete-button"
+            aria-label="delete-button"
+            onClick={onDelete}
+          >
+            <TiDelete className="icon" />
+          </button>
+        )}
 
-      <div className="image">
-        <img src={imageSource} alt={title} />
-      </div>
+        <div className="image">
+          <img src={imageSource} alt={title} />
+        </div>
 
-      <div className="text-info">
-        <div className="title">{title}</div>
-        <div className="description">
-          <div className="left-description">{leftDescription}</div>
-          <div className="right-description">
-            <Button className="primary-button" label={"Ajouter"} />
+        <div className="text-info">
+          <div className="title">{title}</div>
+          <div className="description">
+            <div className="left-description">{leftDescription}</div>
+            <div className="right-description">
+              <Button className="primary-button" label={"Ajouter"} />
+            </div>
           </div>
         </div>
       </div>
@@ -43,106 +50,116 @@ export default function Card({
 }
 
 const CardStyled = styled.div`
-  background: ${theme.colors.white};
-  box-sizing: border-box;
-  width: 240px;
-  height: 330px;
-  display: grid;
-  grid-template-rows: 65% 1fr;
-  padding: 20px;
-  padding-bottom: 10px;
-  box-shadow: ${theme.shadows.medium};
-  border-radius: ${theme.borderRadius.extraRound};
-  position: relative;
-  cursor: pointer;
-
-  .delete-button {
-    /* border: 1px solid red; */
-    position: absolute;
-    right: 15px;
-    top: 15px;
-    width: 30px;
-    height: 30px;
-    color: ${theme.colors.primary};
-    cursor: pointer;
-    z-index: 2;
-    border: none;
-    padding: 0;
-    background: none;
-
-    .icon {
-      height: 100%;
-      width: 100%;
-    }
-    :hover {
-      color: ${theme.colors.red};
-    }
-
-    :active {
-      color: ${theme.colors.primary};
-    }
-  }
-  .image {
-    width: 100%;
-    height: auto;
-    margin-top: 30px;
-    margin-bottom: 20px;
-
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: contain;
-    }
-  }
-
-  .text-info {
+  .card {
+    background: ${theme.colors.white};
+    box-sizing: border-box;
+    width: 240px;
+    height: 330px;
     display: grid;
-    grid-template-rows: 30% 70%;
-    padding: 5px;
+    grid-template-rows: 65% 1fr;
+    padding: 20px;
+    padding-bottom: 10px;
+    box-shadow: ${theme.shadows.medium};
+    border-radius: ${theme.borderRadius.extraRound};
+    position: relative;
 
-    .title {
-      margin: auto 0;
-      font-size: ${theme.fonts.size.P4};
-      position: relative;
-      bottom: 10px;
-      font-weight: ${theme.fonts.weights.bold};
-      color: ${theme.colors.dark};
-      text-align: left;
-      white-space: nowrap;
-      overflow: hidden;
-      width: 100%;
-      text-overflow: ellipsis;
-      font-family: "Amatic SC", cursive;
-    }
+    .delete-button {
+      /* border: 1px solid red; */
+      position: absolute;
+      right: 15px;
+      top: 15px;
+      width: 30px;
+      height: 30px;
+      color: ${theme.colors.primary};
+      cursor: pointer;
+      z-index: 2;
+      border: none;
+      padding: 0;
+      background: none;
 
-    .description {
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-
-      .left-description {
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-        font-weight: ${theme.fonts.weights.medium};
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        font-weight: ${theme.fonts.weights.medium};
-        color: ${theme.colors.primary};
+      .icon {
+        height: 100%;
+        width: 100%;
+      }
+      :hover {
+        color: ${theme.colors.red};
       }
 
-      .right-description {
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-        font-size: ${theme.fonts.size.P1};
+      :active {
+        color: ${theme.colors.primary};
+      }
+    }
+    .image {
+      width: 100%;
+      height: auto;
+      margin-top: 30px;
+      margin-bottom: 20px;
 
-        .primary-button {
-          font-size: ${theme.fonts.size.XS};
-          cursor: pointer;
-          padding: 12px;
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+      }
+    }
+
+    .text-info {
+      display: grid;
+      grid-template-rows: 30% 70%;
+      padding: 5px;
+
+      .title {
+        margin: auto 0;
+        font-size: ${theme.fonts.size.P4};
+        position: relative;
+        bottom: 10px;
+        font-weight: ${theme.fonts.weights.bold};
+        color: ${theme.colors.dark};
+        text-align: left;
+        white-space: nowrap;
+        overflow: hidden;
+        width: 100%;
+        text-overflow: ellipsis;
+        font-family: "Amatic SC", cursive;
+      }
+
+      .description {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+
+        .left-description {
+          display: flex;
+          justify-content: flex-start;
+          align-items: center;
+          font-weight: ${theme.fonts.weights.medium};
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          font-weight: ${theme.fonts.weights.medium};
+          color: ${theme.colors.primary};
+        }
+
+        .right-description {
+          display: flex;
+          justify-content: flex-end;
+          align-items: center;
+          font-size: ${theme.fonts.size.P1};
+
+          .primary-button {
+            font-size: ${theme.fonts.size.XS};
+            cursor: pointer;
+            padding: 12px;
+          }
         }
       }
     }
   }
 `;
+
+// const hoverableStyle = css`
+//   :hover {
+//     transform: scale(1.5);
+//     transition: ease-out 0.4s;
+//     box-shadow: ${theme.shadows.orangeHighLight};
+//     cursor: pointer;
+//   }
+// `;
