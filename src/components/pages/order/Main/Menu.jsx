582a47aa9d5f11bd92c686d4e4eaa17b22ac0019
SniOpy <soufianeibna@gmail.com>
2025-02-04 13:25:07 +0100

    💄 F09-test: CARD > Add selectedStyle [price, delete-button & primary-button]

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-
 import { theme } from "../../../../theme";
 import { formatPrice } from "../../../../utils/maths";
 import Card from "../../../reusable-ui/Card";
