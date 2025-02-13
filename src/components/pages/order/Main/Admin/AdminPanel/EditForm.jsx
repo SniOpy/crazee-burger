c325d8efbe529d6d921a03dc8bb94bdb1bd77ba7
SniOpy <soufianeibna@gmail.com>
2025-02-14 00:41:57 +0100

    ♻️ F09: ADMIN > Form > Use of children props to make it reusable

--- src/components/pages/order/Main/Admin/AdminPanel/EditForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/EditForm.jsx
@@ -1,17 +1,12 @@
 import React, { useContext } from "react";
 import OrderContext from "../../../../../../context/OrderContext";
-import TextInput from "../../../../../reusable-ui/TextInput";
-import ImagePreview from "./ImagePreview.jsx";
-import { getInputTextConfig } from "./inputTextConfig.jsx";
-import styled from "styled-components";
 import EmptyTabEdit from "../Empty/EmptyTab/EmptyTabEdit.jsx";
-import { theme } from "../../../../../../theme/index.js";
+import Form from "./Form.jsx";
 
 export default function EditForm() {
   const { productClicked, setProductClicked, handleEdit, titleCardRef } =
     useContext(OrderContext);
 
-  const inputTexts = getInputTextConfig(productClicked);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -26,69 +21,8 @@ export default function EditForm() {
   return !productClicked ? (
     <EmptyTabEdit />
   ) : (
-    <EditFormStyled>
-      <ImagePreview
-        imageSource={productClicked.imageSource}
-        title={productClicked.title}
-      />
-      <div className="input-fields">
-        {inputTexts.map((input) => {
-          return (
-            <TextInput
-              {...input}
-              onChange={handleChange}
-              version="minimalist"
-              ref={input.name === "title" ? titleCardRef : null}
-            />
-          );
-        })}
-      </div>
-      <div className="submit">
-        <div className="sentence">
-          <span >Cliquez sur un produit pour le modifier <span className="live-update">en temps réel</span>
-            
-          </span>
-        </div>
-      </div>
-      
-    </EditFormStyled>
+    <Form onChange={handleChange} product={productClicked} ref={titleCardRef}>
+      <span className="sentence">Cliquez sur un produit pour le modifier <span className="live-update">en temps réel</span></span>
+    </Form>
   );
 }
-const EditFormStyled = styled.form`
-  padding: 10px 5%;
-  display: grid;
-  height: 100%;
-  width: 70%;
-  grid-template-rows: repeat(4, 1fr);
-  grid-template-columns: 1fr 3fr;
-  grid-column-gap: 20px;
-  grid-row-gap: 8px;
-  padding-top: 30px;
-
-  .input-fields {
-    grid-area: 1 / 2 / 3 / 3;
-    display: grid;
-    grid-row-gap: 8px;
-  }
-  .submit {
-    grid-area: 3 / 2 / 3 / -1;
-    display: flex;
-    align-items: center;
-    gap: 5px;
-    color: ${theme.colors.primary};
-    font-size: ${theme.fonts.size.SM};
-
-    .submit-button {
-      width: 50%;
-    }
-
-    .sentence {
-      color: ${theme.colors.primary};
-      font-size: ${theme.fonts.size.SM};
-
-      .live-update {
-        text-decoration: underline;
-      }
-    }
-  }
-`;
