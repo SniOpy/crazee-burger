c12eddaa1fbf030d079cf25e3bcebc13bbdb4a40
SniOpy <soufianeibna@gmail.com>
2025-02-16 03:46:16 +0100

    💄 F10-test: MAIN > BASKET > improved css on the basket

--- src/components/pages/order/Main/Admin/Basket.jsx
+++ src/components/pages/order/Main/Admin/Basket.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { theme } from '../../../../../theme';
 import styled from 'styled-components';
-import { FaHeart } from 'react-icons/fa';
 
 export default function Basket() {
   return (
@@ -14,78 +13,54 @@ export default function Basket() {
               <div className="contain-cart">Votre commande est vide.</div>
             </div>
             <div className="message-react">
-              <div>Codé avec <span className="heart"><FaHeart />
-              </span> et React.JS</div>
-              </div>
+              <div>Codé avec ❤️ et React.JS</div>
+            </div>
           </BasketStyled>
   )
 }
 
 
 const BasketStyled = styled.div`
-  border-bottom-left-radius: ${theme.borderRadius.extraRound};
+    /* border-bottom-left-radius: ${theme.borderRadius.extraRound}; */
     font-family: 'Amatic SC', cursive;
     font-size: ${theme.fonts.size.P4};
     font-weight: ${theme.fonts.weights.regular};
-    position:relative;
+    display:flex;
+    flex-direction: column;
 
-    .header{
+    .header  {
+      background:${theme.colors.background_dark};
+      color:${theme.colors.primary};
+
       display:flex;
       justify-content: space-between;
-      text-align: center;
+      align-items: center;
+      padding : 0 16px;
+      height: 70px;
 
-      position:absolute;
-      top:0;
-      left:0;
-      right: 0;
-      height:70px;
-      padding: 10px;
-
-      background :${theme.colors.dark};
-      color:${theme.colors.primary};
     }
-
-    .cart {
-      display:grid;
+    .cart  {
+      flex:1;
+      display:flex;
       justify-content: center;
       align-items: center;
-
-      position:absolute;
-      top:70px;
-      left:0;
-      right: 0;
-      height: 694px;
-      
-      box-shadow: ${theme.shadows.strong};;
-      background:${theme.colors.background_white};
-      color: ${theme.colors.greyBlue};
-      font-size: ${theme.fonts.size.P4};
-      line-height: 72px;
-      letter-spacing: 0%;
-      text-align: center;
+      box-shadow: ${theme.shadows.strong};
+      color: ${theme.colors.greyBlue}
     }
 
     .message-react {
-      background:${theme.colors.dark};
+      background:${theme.colors.background_dark};
       color:${theme.colors.white};
-      position:absolute;
-      bottom:0;
-      left:0;
-      right: 0;
-      height:70px;
 
-      display:grid;
+      height: 70px; 
+      display:flex;
       justify-content: center;
-      align-items:center;
-      font-weight: 700;
-      font-size: 20px;
-      line-height: 25.22px;
-      letter-spacing: 0%;
-      border-bottom-left-radius: ${theme.borderRadius.extraRound};
+      align-items: center;
 
+      font-size: ${theme.fonts.size.P3};
 
-      .heart{
-        color: ${theme.colors.redSecondary}
-      }
+      border-bottom-left-radius: ${theme.borderRadius.extraRound}
     }
+
+    
 `;
