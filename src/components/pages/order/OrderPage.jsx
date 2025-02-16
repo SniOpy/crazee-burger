543ac411b09d75e52618f833de6022fc4119d45a
SniOpy <soufianeibna@gmail.com>
2025-02-16 18:54:51 +0100

    ♻️ F10-test: ORDER > Create a hook custom to include state handler

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -5,9 +5,8 @@ import { theme } from "../../../theme";
 import Main from "./Main/Main";
 import Navbar from "./Navbar/Navbar";
 import OrderContext from "../../../context/OrderContext";
-import { fakeMenu } from "../../../fakeData/fakeMenu";
 import { EMPTY_PRODUCT } from "../../../enums/product";
-import { deepClone } from "../../../utils/array";
+import { useMenu } from "../../../hooks/useMenu";
 
 export default function OrderPage() {
   //! state
@@ -15,54 +14,10 @@ export default function OrderPage() {
   const [isModeAdmin, setIsModeAdmin] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [currentTabSelected, setCurrentTabSelected] = useState("add");
-  const [menu, setMenu] = useState(fakeMenu.LARGE);
   const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT);
   const [productClicked, setProductClicked] = useState("");
-
   const titleCardRef = useRef();
-
-  //! comportements
-
-  const handleDelete = (productId) => {
-    // 1. copie du state
-    const menuCopy = deepClone(menu);
-
-    // 2. manipulation de la copie
-    const menuUpdated = menuCopy.filter((product) => product.id !== productId);
-
-    // 3. update du state
-    setMenu(menuUpdated);
-  };
-
-  const handleEdit = (productEdited) => {
-    //1. copy state
-    const menuCopy = deepClone(menu);
-
-    // 2. State manipulation
-    const productIndex = menuCopy.findIndex(
-      (product) => product.id === productEdited.id
-    );
-
-    menuCopy[productIndex] = productEdited;
-
-    // 3. State update
-    setMenu(menuCopy);
-  };
-
-  const resetMenu = () => {
-    setMenu(fakeMenu.MEDIUM);
-  };
-
-  const addProduct = (newProduct) => {
-    //1. Copie du state
-    const copyMenu = deepClone(menu);
-
-    //2. Manipulation du state
-    const updatedMenu = [newProduct, ...copyMenu];
-
-    //3. Nouveau state
-    setMenu(updatedMenu);
-  };
+ const {menu, handleDelete, handleEdit, resetMenu, addProduct} = useMenu()
 
   const orderContextValue = {
     isModeAdmin,
---
+++ src/hooks/useMenu.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import {fakeMenu} from '../fakeData/fakeMenu'
+import {deepClone} from '../utils/array'
+
+export const useMenu = () => {
+
+    const [menu, setMenu] = useState(fakeMenu.LARGE);
+
+
+    //! comportements
+  
+    const handleDelete = (productId) => {
+      // 1. copie du state
+      const menuCopy = deepClone(menu);
+  
+      // 2. manipulation de la copie
+      const menuUpdated = menuCopy.filter((product) => product.id !== productId);
+  
+      // 3. update du state
+      setMenu(menuUpdated);
+    };
+  
+    const handleEdit = (productEdited) => {
+      //1. copy state
+      const menuCopy = deepClone(menu);
+  
+      // 2. State manipulation
+      const productIndex = menuCopy.findIndex(
+        (product) => product.id === productEdited.id
+      );
+  
+      menuCopy[productIndex] = productEdited;
+  
+      // 3. State update
+      setMenu(menuCopy);
+    };
+  
+    const resetMenu = () => {
+      setMenu(fakeMenu.MEDIUM);
+    };
+  
+    const addProduct = (newProduct) => {
+      //1. Copie du state
+      const copyMenu = deepClone(menu);
+  
+      //2. Manipulation du state
+      const updatedMenu = [newProduct, ...copyMenu];
+  
+      //3. Nouveau state
+      setMenu(updatedMenu);
+    };
+
+    return {menu, handleDelete, handleEdit, resetMenu, addProduct}
+};
