b2f381e016fd2e06f0e40cd874f1941c90591c47
SniOpy <soufianeibna@gmail.com>
2025-02-18 16:11:42 +0100

    💄 F11-test: BASKET > PRODUCTS > Display product in cart

--- src/components/pages/order/Main/Admin/Basket/BasketBody.jsx
+++ src/components/pages/order/Main/Admin/Basket/BasketBody.jsx
@@ -1,45 +1,97 @@
-
-import styled from 'styled-components';
-import EmptyBasket from './EmptyBasket';
-import { theme } from '../../../../../../theme';
+import styled from "styled-components";
+import { theme } from "../../../../../../theme";
 
 export default function BasketBody() {
   return (
     <BasketBodyStyled className="cart">
-        {/* <EmptyBasket/> */}
-        <div className="products">
-          <div className="image">Image</div>
-          <div className="body">
-            <div className="title">Title</div>
-            <div className="price">Price</div>
-          </div>
-          <div className="quantity">Quantity</div>
+      {/* <EmptyBasket/> */}
+      <div className="products">
+        <div className="left-side">
+          <img
+            className="image"
+            src="../../../../../../../public/images/burger1.png"
+            alt="IMAGE"
+          />
         </div>
+
+        <div className="right-side">
+          <div className="information">
+            <div className="title">BURGER SMOKE BBQ</div>
+            <div className="price">5,60 €</div>
+          </div>
+          <div className="quantity">x 13</div>
+        </div>
+      </div>
     </BasketBodyStyled>
-  )
+  );
 }
 
 const BasketBodyStyled = styled.div`
-     flex:1;
-     display:flex;
-     justify-content: center;
-     align-items: center; 
-     box-shadow: ${theme.shadows.strong};
-      color: ${theme.colors.greyBlue};
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: ${theme.shadows.strong};
+  color: ${theme.colors.greyBlue};
+  position: relative;
 
-      .products {
-        border:1px solid red;
-        flex:1;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
+  .products {
+    /* border: 1px solid red; */
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    margin: 20px 16px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    box-shadow: -4px 4px 15px 0px rgba(0, 0, 0, 0.2);
+    border-radius: 5px;
+    padding: 8px 16px;
 
+    .left-side {
+      /* border: 1px solid blue; */
+
+      .image {
+        object-fit: contain;
+        object-position: center;
+        height: 75px;
+        width: 85px;
+      }
+    }
+
+    .right-side {
+      /* border: 1px solid green; */
+
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      width: 200px;
+      padding: 25px 20.43px;
+      gap: 6px;
+
+      .information {
+        display:flex;
+        flex-direction:column;
+        gap: 5px 10px;
+
+        .title {
+          font-family: Amatic SC;
+          font-weight: 700;
+          font-size: 24px;
+          color: rgba(23, 22, 26, 1);
+        }
       }
 
-      .body {
-        
+      .price,
+      .quantity {
+        font-family: Open Sans;
+        font-weight: 400;
+        font-size: 15px;
+        color: rgba(255, 160, 27, 1);
       }
-
-      
+    }
+  }
 `;
