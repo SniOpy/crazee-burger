c4a2c44ee7673053b889835dae55b75b2a543ff2
SniOpy <soufianeibna@gmail.com>
2025-02-20 22:33:14 +0100

    ♻️ F11: import final code F10

--- src/components/pages/order/Main/Admin/Basket/Basket.jsx
+++ src/components/pages/order/Main/Admin/Basket/Basket.jsx
@@ -1,27 +1,21 @@
-import React, { useState } from "react";
-import { theme } from "../../../../../../theme";
-import styled from "styled-components";
-import Footer from "./Footer";
-import Total from "./Total";
-import BasketBody from "./BasketBody";
+import styled from "styled-components"
+import Total from "./Total"
+import { formatPrice } from "../../../../../../utils/maths"
+import Footer from "./Footer"
+import BasketBody from "./BasketBody"
 
 export default function Basket() {
-  
-
   return (
     <BasketStyled>
-      <Total amountToPay={5.3655} />
+      <Total amountToPay={formatPrice(0)} />
       <BasketBody />
       <Footer />
     </BasketStyled>
-  );
+  )
 }
 
 const BasketStyled = styled.div`
+  background: pink;
   display: flex;
   flex-direction: column;
-  font-family: "Amatic SC", cursive;
-  font-size: ${theme.fonts.size.P4};
-  font-weight: ${theme.fonts.weights.regular};
-  position: relative;
-`;
+`
--- src/components/pages/order/Main/Admin/Basket/Footer.jsx
+++ src/components/pages/order/Main/Admin/Basket/Footer.jsx
@@ -1,29 +1,27 @@
-import React from "react";
-import styled from "styled-components";
-import { theme } from "../../../../../../theme";
+import styled from "styled-components"
+import { theme } from "../../../../../../theme"
+import Header from "../../../../../reusable-ui/Header.jsx"
 
 export default function Footer() {
   return (
-    <FooterStyled className="message-react">
-      <div>Codé avec ❤️ et React.JS</div>
-    </FooterStyled>
-  );
+    <Header>
+      <FooterStyled>
+        <span>Codé avec ❤️ et React.JS</span>
+      </FooterStyled>
+    </Header>
+  )
 }
 
 const FooterStyled = styled.div`
-  background: ${theme.colors.background_dark};
-  color: ${theme.colors.white};
-
-  height: 70px;
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
 
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-
-  font-size: ${theme.fonts.size.P3};
-  border-bottom-left-radius: ${theme.borderRadius.extraRound};
-`;
+  span {
+    font-size: ${theme.fonts.size.P2};
+    font-family: ${theme.fonts.family.stylish};
+    font-weight: ${theme.fonts.weights.bold};
+    color: ${theme.colors.white};
+  }
+`
