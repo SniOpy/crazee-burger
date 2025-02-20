92e6b00f73e62e29d3635f9a0e3a0d6cc3513c58
SniOpy <soufianeibna@gmail.com>
2025-02-20 22:33:14 +0100

    ♻️ F11: import final code F10

--- src/components/pages/order/Main/Admin/Basket/BasketBody.jsx
+++ src/components/pages/order/Main/Admin/Basket/BasketBody.jsx
@@ -1,42 +1,31 @@
-import styled from "styled-components";
-import { theme } from "../../../../../../theme";
-// import { fakeBasket } from "../../../../../../fakeData/fakeBasket";
-import {  useState } from "react";
-import EmptyBasket from "./EmptyBasket";
-import Product from "./Product";
+import React from "react"
+import styled from "styled-components"
+import { theme } from "../../../../../../theme"
 
 export default function BasketBody() {
-  
-  const [orders, setOrders] = useState([]);
-
- 
-
   return (
     <BasketBodyStyled>
-      {orders ? (
-        orders.map((order) => {
-          return (
-            <Product order={order} isHoverable={isModeAdmin} key={order.id}/>
-          );
-        })
-      ) : (
-        <EmptyBasket />
-      )}
+      <span className="empty-message">Votre commande est vide.</span>
     </BasketBodyStyled>
-  );
+  )
 }
 
 const BasketBodyStyled = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: stretch;
-  row-gap: 16px;
-  box-shadow: ${theme.shadows.strong};
-  color: ${theme.colors.greyBlue};
-  height: 668.9px; //! initialement 694.69px
-  overflow-y: scroll;
-  padding-top: 10px;
-  padding-bottom: 10px;
-  
-`;
+  flex: 1;
+  background: ${theme.colors.background_white};
+  box-shadow: ${theme.shadows.basket};
 
+  .empty-message {
+    display: flex;
+    height: calc(95vh - 10vh - 70px - 70px);
+    text-align: center;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    align-self: center;
+    line-height: 2;
+    font-family: ${theme.fonts.family.stylish};
+    font-size: ${theme.fonts.size.P4};
+    color: ${theme.colors.greyBlue};
+  }
+`
