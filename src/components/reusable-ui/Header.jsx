a2b4d4765447d613ff5df61cd93f89918865f738
SniOpy <soufianeibna@gmail.com>
2025-03-02 00:33:20 +0000

    ✨ F11: BASKET & MENU > Add product with his quantity & delete product menu with delete on basket

--- src/components/reusable-ui/Header.jsx
+++ src/components/reusable-ui/Header.jsx
@@ -1,15 +1,14 @@
-import React from "react"
-import styled from "styled-components"
-import { theme } from "../../theme"
+import React from "react";
+import styled from "styled-components";
+import { theme } from "../../theme";
 
 export default function Header({ children }) {
-  return <HeaderStyled>{children}</HeaderStyled>
+  return <HeaderStyled>{children}</HeaderStyled>;
 }
 
 const HeaderStyled = styled.div`
   height: 70px;
   background: ${theme.colors.background_dark};
   padding: 0 16px;
-
-  
-`
+  border-bottom-left-radius: 5px;
+`;
